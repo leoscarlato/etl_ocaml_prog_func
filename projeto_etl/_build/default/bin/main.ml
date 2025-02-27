f2fc44fc43133c92edd6beddb88ab57ee5e90200
Leonardo Scarlato <leonardo.scarlato@hotmail.com>
2025-02-27 11:27:18 -0300

    refat

--- projeto_etl/_build/default/bin/main.ml
+++ projeto_etl/_build/default/bin/main.ml
@@ -1,11 +1,4 @@
 open Etl_lib.Order_functions
-
-(* let () =
-  print_endline (Etl.hello_etl()) *)
-
-let string_of_order order =
-  Printf.sprintf "Pedido %d, Cliente %d, Data: %s, Status: %s, Origem: %s"
-    order.id order.client_id order.order_date order.status order.origin
  
 let () =
   let orders = read_orders "data/order.csv" in
